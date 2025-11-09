f05eb260a90889b9ac49fa46d34c90cbee86475a
gregolly <gregollyff@hotmail.com>
2025-11-09 12:24:53 -0400

    styles: change classes

--- template-parts/section-contact.php
+++ template-parts/section-contact.php
@@ -13,10 +13,10 @@ $title = get_field('contato_titulo', 'option');
                 ?>
             </div>
 
-            <div class="lg:w-1/2 flex flex-col lg:items-start lg:text-right lg:items-end">
+            <div id="right-faq" class="lg:w-1/2 flex flex-col lg:items-start lg:text-right lg:items-end">
                 
                 <?php if ($title) : ?>
-                    <h2 class="flextext-4xl font-serif text-brand-azul-escuro">
+                    <h2 class="flex text-4xl font-serif text-brand-azul-escuro">
                         <?php echo esc_html($title) ?>
                     </h2>
                     <div class="w-16 h-0.5 bg-brand-marrom my-6"></div>
@@ -36,8 +36,8 @@ $title = get_field('contato_titulo', 'option');
                             $icon = get_sub_field('social_icon');
                             $url = get_sub_field('social_url');
                         ?>
-                            <a href="<?php echo esc_url($url); ?>" target="_blank" 
-                               class="w-12 h-12 bg-brand-marrom text-white p-3 rounded-full hover:bg-brand-azul-escuro transition-colors">
+                            <a href="<?php echo esc_url($url); ?>" target="_blank"
+                               class="w-12 h-12 bg-brand-marrom text-white p-3 hover:opacity-50 transition-colors">
                                 <?php if ($icon): ?>
                                     <img src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>" class="w-full h-full object-contain">
                                 <?php endif; ?>
