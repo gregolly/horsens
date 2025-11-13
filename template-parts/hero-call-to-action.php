b8e9420a510a0c8b67cafdbe9ffa94af7bca68a4
gregolly <gregollyff@hotmail.com>
2025-11-13 11:56:01 -0400

    feat(template-cta): add some conditionals code and news html tags.

--- template-parts/hero-call-to-action.php
+++ template-parts/hero-call-to-action.php
@@ -1,10 +1,34 @@
-<?php 
-// Template Part: Call to action
+<?php
+/**
+ * Template Part: Call to Action
+ */
 ?>
-<div class="call-to-action absolute left-0 md:left-40 mt-4 text-wrap md:w-[500px] px-2">
-    <h2 class="text-3xl md:text-4xl mb-2 text-brand-azul-escuro"><?php the_field('hero_titulo', 'option'); ?></h2>
-    <h3 class="text-brand-azul-escuro font-sans md:w-[400px]"><?php the_field('hero_subtitulo', 'option'); ?></h3>
-    <a href="<?php the_field('hero_link', 'option'); ?>" class="block mt-4 bg-brand-marrom text-center p-3 max-w-full mr-0 md:mr-1 md:w-[240px] font-sans hover:bg-brand-off-white hover:text-brand-azul-escuro ease-in duration-300">
-        <?php the_field('hero_label_button', 'option'); ?>
-    </a>
+
+<div class="relative z-5 lg:pt-24 px-4 pt-4 md:pt-8 lg:ml-20" data-scroll-reveal>
+    
+    <?php if ( is_front_page() ) : ?>
+
+        <h1 class="lg:text-4xl text-3xl font-serif text-brand-azul-escuro lg:w-[500px]">
+            <?php the_field('titulo_hero_home', 'option'); // Ajuste o nome do campo se necessário ?>
+        </h1>
+        <p class="text-1xl md:text-2xl text-brand-azul-escuro mt-4 font-sans mb-8 lg:w-[600px]">
+            <?php the_field('subtitulo_hero_home', 'option'); // Ajuste o nome do campo se necessário ?>
+        </p>
+        <a href="<?php the_field('hero_link', 'option'); ?>" class="mt-4 bg-brand-marrom hover:bg-brand-azul-escuro text-dark hover:text-brand-off-white transition-colors px-14 py-4 font-sans">
+            <?php the_field('hero_label_button', 'option'); ?>
+        </a>
+
+    <?php elseif (is_single('equipe')) : ?>
+        <h1 class="text-4xl md:text-6xl font-serif text-white text-center">
+            Detalhes da Instrutora
+        </h1>
+
+    <?php else : ?>
+
+        <h1 class="text-4xl md:text-6xl font-serif text-white text-center">
+            <?php the_title(); ?>
+        </h1>
+
+    <?php endif; ?>
+
 </div>
