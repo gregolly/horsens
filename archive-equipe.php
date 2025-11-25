01b618969d8a0301084a37de726fc041b98f1724
gregolly <gregollyff@hotmail.com>
2025-11-24 22:56:42 -0400

    archive equipe page

---
+++ archive-equipe.php
@@ -0,0 +1,13 @@
+<?php 
+get_header();
+?>
+
+
+
+<?php get_template_part('template-parts/section-staff'); ?>
+<?php get_template_part('template-parts/banner'); ?>
+<?php get_template_part('template-parts/section-faq'); ?>
+
+<?php 
+get_footer();
+?>
