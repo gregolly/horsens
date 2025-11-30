ac3c05c5bff2327183c9fa986c9ac33f4eb51fcc
gregolly <gregollyff@hotmail.com>
2025-11-30 18:51:47 -0400

    remove metatag

--- header-alt.php
+++ header-alt.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset=”<?php bloginfo('charset'); ?>”>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="<?php bloginfo('description'); ?>" />
+
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <?php wp_head(); ?>
 </head>
